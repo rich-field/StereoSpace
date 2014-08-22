dafbce2a3385cb7b293e50c4269d5c457089c8ee
lyntco <strawww@gmail.com>
2014-08-22 10:31:57 +1000

    tests

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -2,14 +2,28 @@ require 'rails_helper'
 
 RSpec.describe SongsController, :type => :controller do
   before do
-    3.times do
-      Song.create
+    3.times do |i|
+      song = Song.new(:share_url => "dsauyufihkdsjhfias#{i}")
+      song.save
     end
   end
 
-  describe '.index' do
+  describe 'GET /songs' do
+    before do
+      get :index
+    end
     it 'should assign @songs to all songs' do
-      expect( assigns(:songs) ).to eq(3)
+      expect( assigns(:songs).count ).to eq(3)
     end
   end
+
+  describe 'CREATE /songs' do
+    before do
+      get :index
+    end
+    it 'should assign @songs to all songs' do
+      expect( assigns(:songs).count ).to eq(3)
+    end
+  end
+
 end
